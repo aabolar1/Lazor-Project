4ba539c964c26bbace3fe63e64d1bf527ac59cd0
mariejkim <129775886+mariejkim@users.noreply.github.com>
2023-04-11 16:16:33 -0400

    Add files via upload

--- lazor_JYK.py
+++ lazor_JYK.py
@@ -1,101 +1,147 @@
 import random
 
-def LazorBoard(filename):
+class Board:
     '''
-    This function reads in the contents of the .bff file and creates a representative board where each cell represents a position on the block
-    and contains information about the block located there.
+    This class object represents the game board, including its dimensions and the blocks, and lazors.
     '''
-    # Split the contents of the file into lines
-    with open(filename, 'r') as bff:
-        lines = bff.readlines()
-     
-    # Create the board with a step size of a half block, so odd numbers are space between the blocks and even numbers are the actual block
-    pre_board = []
-    board = []
-    for line in lines:
-        line = line.strip()
-        
-        if line.startswith("GRID START"):
-            continue
-        elif line.startswith("GRID STOP"):
-            break
-        else:
-            row = []
-            for char in line:
-                if char in ["o", "x", "A", "B", "C"]:
-                    row.append(char)
-                else:
-                    row.append(" ")
-            pre_board.append(row)
-        
-        if line.startswith("L"):
-            lazor_point
-            
-            
-    rows = (len(pre_board) * 2) - 1
-    index = 0
-    for row in range(rows):
-        if row == 0:
-            board.append(pre_board[row])
-            index += 1
-        elif row % 2 == 0:
-            board.append(pre_board[index])
-            index += 1
-        else:
-            empty_row = [" "] * len(pre_board[0])
-            board.append(empty_row) 
-            
-    return board
-
-#board = LazorBoard("yarn_5.bff")
-
-def board_state(filename, board):
-    '''
-    This function stores the current state of the board as a 2D array, where each cell represents a position on the 
-    board and contains information about the blocks located there.
-    '''
-    # Split the contents of the file into lines
-    with open(filename, 'r') as bff:
-        lines = bff.readlines()
+    def __init__(self, filename):
+        '''
+        Initializes a new Board object from a .bff file.
+        '''
+        self.filename = filename
+        self.board = self.read_Board()
+        self.lazors = self.get_lazors() 
+        self.targets = self.get_targets()
     
-    new_board = [row[:] for row in board]
-    start_parsing = False
-    
-    for line in lines:
-        line = line.strip()
+    def read_Board(self):
+        '''
+        This function reads in the contents of the .bff file and creates a representative board where each cell represents a position on the 
+        block and contains information about the block located there.
+        '''
+        # Split the contents of the file into lines
+        with open(self.filename, 'r') as bff:
+            lines = bff.readlines()
+
+        # Create the board with a step size of a half block, so odd numbers are space between the blocks and even numbers are the actual block
+        pre_board = []
+        board = []
+        for line in lines:
+            line = line.strip()
+
+            if line.startswith("GRID START"):
+                continue
+            elif line.startswith("GRID STOP"):
+                break
+            else:
+                row = []
+                for char in line:
+                    if char in ["o", "x", "A", "B", "C"]:
+                        row.append(char)
+                    else:
+                        row.append(" ")
+                pre_board.append(row)
+
+        rows = (len(pre_board) * 2) - 1
+        index = 0
+        for row in range(rows):
+            if row == 0:
+                board.append(pre_board[row])
+                index += 1
+            elif row % 2 == 0:
+                board.append(pre_board[index])
+                index += 1
+            else:
+                empty_row = [" "] * len(pre_board[0])
+                board.append(empty_row) 
+        return board
+
+    def board_state(self):
+        '''
+        This function stores the current state of the board as a 2D array, where each cell represents a position on the 
+        board and contains information about the blocks located there.
+        '''
+        # Split the contents of the file into lines
+        with open(self.filename, 'r') as bff:
+            lines = bff.readlines()
+
+        # Cope the board to a new list to not modify the original board
+        new_board = [row[:] for row in self.board]
         
-        if line == "GRID STOP":
-            start_parsing = True
-            continue
-            
-        if start_parsing and (line.startswith("A") or line.startswith("B") or line.startswith("C")):
-            # Parse the block type and amount
-            block_type, amount = line[0], int(line[2])
-            
-            # Find the empty spots where blocks are allowed
-            empty_spots = [(i, j) for i in range(len(board)) for j in range(len(board[0])) if board[i][j] == "o"]
-            if len(empty_spots) < int(amount):
-                raise ValueError(f"Warning: Not enough empty spots for {block_type}")
-            
-            # Add blocks to the board through random distribution
-            random.shuffle(empty_spots)
-            for i in range(amount):
-                x, y = empty_spots[i]
-                new_board[x][y] = block_type
-                
-    # Adding blanks around the board (Temporary, can be added to the board_state function)            
-    empty_row = [" "] * (len(board[0])+2)
-    for list in new_board:
-        list.insert(0," ")
-        list.append(" ")
-    new_board.insert(0,empty_row)
-    new_board.append(empty_row)
-    #board_state[3][5] = "C"
-            
-    return new_board
+        
+        # Flag to start parsing the blocks in the file
+        start_parsing = False
 
-#board_state = board_state("yarn_5.bff", board)
+        for line in lines:
+            line = line.strip()
 
+            if line == "GRID STOP":
+                start_parsing = True
+                continue
+
+            if start_parsing and (line.startswith("A") or line.startswith("B") or line.startswith("C")):
+                # Parse the block type and amount
+                block_type, amount = line[0], int(line[2])
+
+                # Find the empty spots where blocks are allowed
+                empty_spots = [(i, j) for i in range(len(new_board)) for j in range(len(new_board[0])) if new_board[i][j] == "o"]
+                if len(empty_spots) < int(amount):
+                    raise ValueError(f"Warning: Not enough empty spots for {block_type}")
+
+                # Add blocks to the board through random distribution
+                random.shuffle(empty_spots)
+                for i in range(amount):
+                    x, y = empty_spots[i]
+                    new_board[x][y] = block_type       
+
+        # Adding blanks around the board to ensure block positions are correct
+        empty_row = [" "] * (len(new_board[0])+2) 
+        new_board = [empty_row] + [[" "] + row + [" "] for row in new_board] + [empty_row]
+         
+        return new_board
+    
+    def get_lazors(self):
+        '''
+        This function reads in the contents of the .bff file and creates a list of lazors that exist on the board, with their starting 
+        position and velocity.
+        '''
+        with open(self.filename, 'r') as bff:
+            lines = bff.readlines()
+
+        lazors = []
+
+        for line in lines:
+            line = line.strip()
+
+            if line.startswith("L "):
+                # Parse the lazor information
+                data = line[2:].split(" ")
+                lazor = [int(i) for i in data]
+
+                lazors.append(lazor)
+
+        return lazors
+    
+    def get_targets(self):
+        '''
+        This function reads in the contents of the .bff file and creates a list of target positions we need the lazers to intersect
+        '''
+        with open(self.filename, 'r') as bff:
+            lines = bff.readlines()
+
+        targets = []
+
+        for line in lines:
+            line = line.strip()
+
+            if line.startswith("P "):
+                # Parse the lazor information
+                data = line[2:].split(" ")
+                target = [int(i) for i in data]
+
+                targets.append(tuple(target))
+
+        return targets
+    
 def pt_chk(x, y, board_state):
     '''
     Validate if the coordinates specified (x and y) are out of the boundaries of the board.
@@ -116,7 +162,7 @@ def pt_chk(x, y, board_state):
     '''
     return (x < 0 or x >= len(board_state[0])) or (y < 0 or y >= len(board_state))
 
-def Lazer_paths(filename,board_state):
+def Lazer_paths(filename,board_state,lazors):
     '''
     
     Parameters
@@ -133,22 +179,25 @@ def Lazer_paths(filename,board_state):
 
     '''
 
-    with open(filename, 'r') as bff:
-        lines = bff.readlines()
+    # with open(filename, 'r') as bff:
+    #     lines = bff.readlines()
     
-    start_points = []
-    for line in lines:
-        line = line.strip()
+    # start_points = []
+    # for line in lines:
+    #     line = line.strip()
         
-        if line.startswith("L"):
-            data = line[2:].split(" ")
-            start_point = [int(i) for i in data]
-        else:   # Why are these 'else' lines neccessary?
-            continue
-        start_points.append(start_point)
-        print(start_points)        
+    #     if line.startswith("L"):
+    #         data = line[2:].split(" ")
+    #         start_point = [int(i) for i in data]
+    #     else:   # Why are these 'else' lines neccessary?
+    #         continue
+    #     start_points.append(start_point)
+        # print(start_points)        
     
-    #start_points.append([4,2,1,1])  
+    # start_points =[[4,5,-1,-1]]
+    start_points = lazors
+    # print("start_points1:",start_points) 
+    
     lazer_paths = []      
     for list in start_points:
         lazer_path = [(list[0],list[1])]
@@ -191,12 +240,13 @@ def Lazer_paths(filename,board_state):
                 else: # "o" or "x" or "C"
                     lazer_direction.append(reflected_dir)
                     lazer_path.append((current_pt[0]+lazer_direction[-1][0], current_pt[1]+lazer_direction[-1][1]))
-     
+                    
+        # print("start_points2:",start_points)                    
     return lazer_paths
 
 #Lazer_Paths = Lazer_paths("yarn_5.bff", board_state)                   
                 
-def target_point_chk(filename, lazer_paths): # Not done yet...
+def target_point_chk(filename, lazer_paths, targets): # Not done yet...
     '''
     A function to check if the laser path goes through the target points
     
@@ -213,24 +263,25 @@ def target_point_chk(filename, lazer_paths): # Not done yet...
         DESCRIPTION.
 
     '''
-    with open(filename, 'r') as bff:
-        lines = bff.readlines()
+    # with open(filename, 'r') as bff:
+    #     lines = bff.readlines()
     
-    target_points = []
-    for line1 in lines:
-        line1 = line1.strip()
+    # target_points = []
+    # for line1 in lines:
+    #     line1 = line1.strip()
         
-        if line1.startswith("P"):
-            target_point = (int(line1[2]),int(line1[4]))
-        else:   # Why are these 'else' lines neccessary?
-            continue
+    #     if line1.startswith("P"):
+    #         target_point = (int(line1[2]),int(line1[4]))
+    #     else:   # Why are these 'else' lines neccessary?
+    #         continue
         
-        target_points.append(target_point)
+    #     target_points.append(target_point)
                
     #print(target_points)
     
     # #lazer_paths = [[(6,9)],[(9,2)]]
     # lazer_paths = [[(1,2),(5,4)], [(6,3)]]
+    target_points = targets
     
     lazer_pts =[]
     for list in lazer_paths:
@@ -250,7 +301,7 @@ def target_point_chk(filename, lazer_paths): # Not done yet...
 def Lazor_solution(filename):
 
     # filename = "yarn_5.bff"
-    board = LazorBoard(filename)
+    board = Board(filename)
 
 # curr_board = board_state(filename, board)
 # # curr_board = [[' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'A', ' ', 'o', ' ', 'A', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'o', ' ', 'o', ' ', 'o', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'A', ' ', 'C', ' ', 'o', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
@@ -260,9 +311,16 @@ def Lazor_solution(filename):
     iteration = 0
     result = True
     while result:
-        curr_board = board_state(filename, board) # board is coming out eith missing blocks
-        lazer_paths = Lazer_paths(filename, curr_board)
-        result = target_point_chk(filename, lazer_paths)
+        # starting_points = []
+        curr_board = board.board_state()
+        # print(curr_board)
+        starting_points = board.lazors.copy()
+        # print("lazors:",lazors)
+        target_points = board.targets
+        print(target_points)
+        lazer_paths = Lazer_paths(filename, curr_board, starting_points)
+        # print(lazer_paths)
+        result = target_point_chk(filename, lazer_paths, target_points)
         iteration += 1
         print(iteration)
         
@@ -272,50 +330,21 @@ def Lazor_solution(filename):
             for i in list:
                 answer_file.write(i)
             answer_file.write('\n')
+            
     return curr_board
 
-answer = Lazor_solution("tiny_5.bff")
+    
+if __name__ == "__main__":    
+    # board = Board("tiny_5.bff")
+    # curr_board = board.board_state()
+    # lazors = board.lazors
+    # print("lazors2:",lazors)
+    # targets = board.targets
+    
+    answer = Lazor_solution("tiny_5.bff")
+    
+    # print(curr_board)
+    # print(lazors)
+    # print(targets)
 
-
-    
-class Block:
-    '''
-    This class object to represent each block within the game. The block object contains information 
-    about the block type (reflect, opaque, or refract), its position on the board and its orientation
-    '''
-    def __init__(self, x, y, block_type):
-        self.x = x
-        self.y = y
-        self.block_type = block_type
-        
-    def get_position(self):
-        '''
-        Returns the current position of the block as a tuple (x, y)
-        '''
-        return (self.x, self.y)
-    
-    def set_position(self, x, y):
-        '''
-        Updates the position of the block to (x, y)
-        '''
-        self.x = x
-        self.y = y
-    
-    def get_type(self):
-        '''
-        Returns the type of the block (reflect, opaque, or refract)
-        '''
-        return self.block_type
-    
-    def set_type(self, block_type):
-        '''
-        Updates the type of the block to block_type
-        '''
-        self.block_type = block_type
-        
-    def __str__(self):
-        '''
-        Returns a string representation of the block
-        '''
-        return f"Block at ({self.x}, {self.y}) of type '{self.block_type}'"
     
