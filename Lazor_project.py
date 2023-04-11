09b5f751baddb7c183409f2ada7510094236f035
mariejkim <129775886+mariejkim@users.noreply.github.com>
2023-04-11 19:33:06 -0400

    Add files via upload

--- Lazor_project.py
+++ Lazor_project.py
@@ -1,22 +1,42 @@
 import random
-
 class Board:
     '''
     This class object represents the game board, the blocks on the board, the lazors and targets.
     '''
     def __init__(self, filename):
         '''
-        Initializes a new Board object from a .bff file.
+        Initializes a new Board object from a .bff file
+
+        Parameters:
+        ----------
+        filename : str
+            The name of the .bff file
+
+        Returns
+        -------
+        None
         '''
         self.filename = filename
-        self.board = self.get_board() # call get_board method to create game board
-        self.lazors = self.get_lazors() # call get_lazors method to create lazor list
-        self.targets = self.get_targets() # call get_targets method to create target list
+
 
     def get_board(self):
         '''
-        This function reads in the contents of the .bff file and creates a representative board where each 
-        cell represents a position on the block and contains relevant information
+        This function reads in the contents of the .bff file and creates a corresponding board 
+        by randomly distributing the assigned blocks
+        
+        Parameters: None
+        ----------
+        
+        Raises
+        ------
+        ValueError
+            If there are more blocks than free space, an error occured when parsing the .bff 
+            file or the file is incorrect.
+
+        Returns
+        -------
+        new_board : list
+            A list of lists that represent the game board.
         '''
         # Split the contents of the bff file into lines
         with open(self.filename, 'r') as bff:
@@ -47,26 +67,29 @@ class Board:
         # Remove the last blank row that was appeneded   
         board.pop()
         
-        # Modify the board based on the blocks specified in the file
-        start_parsing = False   
-
+        # Randomly distribute the blocks onto the board 
+        parse = False
         for line in lines:
             line = line.strip()
-
-            if line.startswith(("A", "B", "C")):
-                # Parse the block type and amount
-                block_type, amount = line[0], int(line[2])
-
-                # Find the empty spots where blocks are allowed
-                empty_spots = [(i, j) for i in range(len(board)) for j in range(len(board[0])) if board[i][j] == "o"]
-                if len(empty_spots) < int(amount):
-                    raise ValueError(f"Warning: Not enough empty spots for {block_type}")
-
-                # Add blocks to the board through random distribution
-                random.shuffle(empty_spots)
-                for i in range(amount):
-                    x, y = empty_spots[i]
-                    board[x][y] = block_type       
+            
+            if line.startswith("GRID STOP"):
+                parse = True
+                continue
+            if parse:
+                if line.startswith(("A", "B", "C")):
+                    # Parse the block type and amount
+                    block_type, amount = line[0], int(line[2])
+    
+                    # Find the empty spots where blocks are allowed
+                    empty_spots = [(i, j) for i in range(len(board)) for j in range(len(board[0])) if board[i][j] == "o"]
+                    if len(empty_spots) < int(amount):
+                        raise ValueError(f"Warning: Not enough empty spots for {block_type}")
+    
+                    # Add blocks to the board through random distribution
+                    random.shuffle(empty_spots)
+                    for i in range(amount):
+                        x, y = empty_spots[i]
+                        board[x][y] = block_type       
 
         # Adding blanks around the board to ensure block positions are correct
         empty_row = [" "] * (len(board[0])+2) 
@@ -76,8 +99,17 @@ class Board:
     
     def get_lazors(self):
         '''
-        This function reads in the contents of the .bff file and creates a list of lazors that exist on the board, with their starting 
-        position and velocity.
+        This function reads in the contents of the .bff file and creates a list 
+        of lazors that exist on the board, with their starting position and velocity.
+        
+        Parameters: None
+        ----------
+        
+        Returns
+        -------
+        lazors : list
+            A list of lists that represent the lazers on the board, where each 
+            sublist contains starting positon and velocity [x, y, vx, vy]
         '''
         lazors = []
         with open(self.filename, 'r') as bff:
@@ -92,69 +124,63 @@ class Board:
     
     def get_targets(self):
         '''
-        This function reads in the contents of the .bff file and creates a list of target positions we need the lazers to intersect
+        This function reads in the contents of the .bff file and identifies the 
+        targets the lazors must pass through to solve the game.
+        
+        Parameters: None
+        ----------
+        
+        Returns
+        -------
+        list:
+            A list of tuples containing the coordinates of each target on the board.
+        
         '''
         with open(self.filename, 'r') as bff:
             lines = bff.readlines()
             
         return [(int(x), int(y)) for line in lines if not line.startswith("#") and line.startswith("P ") for x, y in [line.strip()[2:].split(" ")]]
-
+    
 def pt_chk(x, y, board_state):
+    
     '''
-    Validate if the coordinates specified (x and y) are out of the boundaries of the board.
+    Check if the coordinates specified (x and y) are out of the boundaries of the board.
 
-    **Parameters**
+    Parameters
+    ----------
+    x: *int*
+        An x coordinate to check if it resides ouside the board.
+    y: *int*
+        A y coordinate to check if it resides outside the board.
+    board_state: *list of lists*
+        The board we are looking into?
 
-        x: *int*
-            An x coordinate to check if it resides within the maze.
-        y: *int*
-            A y coordinate to check if it resides within the maze.
-        board_state: *list of lists*
-            The board we are looking into?
-
-    **Returns**
-
-        valid: *bool*
+    Returns
+    -------
+        *bool*
             Whether the coordiantes are out of bound (True) or not (False).
     '''
     return (x < 0 or x >= len(board_state[0])) or (y < 0 or y >= len(board_state))
 
-def Lazer_paths(filename,board_state,lazors):
+def Lazer_paths(filename,board_state,start_points):
     '''
+    Determines the laser path(s) for the given board state (block configuration on the board).
     
     Parameters
     ----------
-    filename : TYPE
-        DESCRIPTION.
-    board_state : TYPE
-        DESCRIPTION.
+    filename : *str*
+        Name of board file.
+    board_state : *List of Lists*
+        Board state with blocks positioned.
+    lazors: *list of lists*
+        All laser starting points (their coordinates and their vectors).
 
     Returns
     -------
-    lazer_paths : *List of Lists*
+    lazer_paths : *list of lists*
         All points the laser passes through.
 
-    '''
-
-    # with open(filename, 'r') as bff:
-    #     lines = bff.readlines()
-    
-    # start_points = []
-    # for line in lines:
-    #     line = line.strip()
-        
-    #     if line.startswith("L"):
-    #         data = line[2:].split(" ")
-    #         start_point = [int(i) for i in data]
-    #     else:   # Why are these 'else' lines neccessary?
-    #         continue
-    #     start_points.append(start_point)
-        # print(start_points)        
-    
-    # start_points =[[4,5,-1,-1]]
-    start_points = lazors
-    # print("start_points1:",start_points) 
-    
+    '''    
     lazer_paths = []      
     for list in start_points:
         lazer_path = [(list[0],list[1])]
@@ -162,153 +188,128 @@ def Lazer_paths(filename,board_state,lazors):
         
         end_flag = 0
         while end_flag == 0:
-            current_pt = lazer_path[-1]
-            current_dir = lazer_direction[-1]
+            current_pt = lazer_path[-1] # Current laser point in laser path.
+            current_dir = lazer_direction[-1] # Current laser point direction is laser path.
             
-            if current_pt[0]%2 == 0: # When lazer is in the vertical intersection of boards, investigate the board left and right the lazer points
-                inv_pt1 = (current_pt[0]+current_dir[0],current_pt[1]) # Investigating point 1: board location the lazer or pointing towards
-                inv_pt2 = (current_pt[0]-current_dir[0],current_pt[1]) # Investigating point 2: board location next to the investigating point
+            if current_pt[0]%2 == 0: 
+            # When lazer is in the vertical intersection of boards, investigate the board left and right the lazer points.
+                inv_pt1 = (current_pt[0]+current_dir[0],current_pt[1]) # Investigating point 1: board location the lazer or pointing towards.
+                inv_pt2 = (current_pt[0]-current_dir[0],current_pt[1]) # Investigating point 2: board location next to the investigating point.
                 reflected_dir = (-current_dir[0], current_dir[1])
-            else: # When lazer is in the horizontal intersection of boards, investigate the board above and below the lazer points
-                inv_pt1 = (current_pt[0],current_pt[1]+current_dir[1]) # Investigating point 1: board location the lazer or pointing towards
-                inv_pt2 = (current_pt[0],current_pt[1]-current_dir[1]) # Investigating point 2: board location next to the investigating point
+            else: 
+            # When lazer is in the horizontal intersection of boards, investigate the board above and below the lazer points.
+                inv_pt1 = (current_pt[0],current_pt[1]+current_dir[1]) # Investigating point 1: board location the lazer or pointing towards.
+                inv_pt2 = (current_pt[0],current_pt[1]-current_dir[1]) # Investigating point 2: board location next to the investigating point.
                 reflected_dir = (current_dir[0], -current_dir[1])    
            
-            if pt_chk(inv_pt1[0], inv_pt1[1], board_state): # When investing positions is out of the board region
+            if pt_chk(inv_pt1[0], inv_pt1[1], board_state): 
+            # When investing position 1 is out of the board region, append current laser path to laser paths list and end path.
                 lazer_paths.append(lazer_path)
                 break 
-            elif board_state[inv_pt1[1]][inv_pt1[0]] == "B":
+            elif board_state[inv_pt1[1]][inv_pt1[0]] == "B": 
+            # When investing position 1 is an opaque block (B), append current laser path to laser paths list and end path. 
                 lazer_paths.append(lazer_path)
                 break
-            elif board_state[inv_pt1[1]][inv_pt1[0]] == "o" or board_state[inv_pt1[1]][inv_pt1[0]] == "x":
-                lazer_direction.append(current_dir)
-                lazer_path.append((current_pt[0]+current_dir[0], current_pt[1]+current_dir[1]))
-            else: # If block is "A" or "C"
+            elif board_state[inv_pt1[1]][inv_pt1[0]] == "o" or board_state[inv_pt1[1]][inv_pt1[0]] == "x": 
+            # When investigating position 1 is either an available (o) or unavailable blank (x)
+                lazer_direction.append(current_dir) # Next laser point would have the same direction.
+                lazer_path.append((current_pt[0]+current_dir[0], current_pt[1]+current_dir[1])) # Next laser point position.
+                if len(lazer_path) > 50: 
+                    # If the lazer path length is higher than 50, it assumes that it is in an infinite loop and breaks out of the loop
+                    lazer_paths.append(lazer_path)
+                    break
+            else: # When investigating position 1 has either a reflective (A) of refractive (C) block
                 
                 if board_state[inv_pt1[1]][inv_pt1[0]] == "C":
+                # When investigating position is a refractive block (C), create a new start point as a new laser path branch is formed.
+                # The new starting point would be the next position in the current direction with the direction inchanged.
                     start_points.append([current_pt[0] + current_dir[0], current_pt[1] + current_dir[1], current_dir[0], current_dir[1]])
                     
                 if pt_chk(inv_pt2[0], inv_pt2[1], board_state):
+                # When investigating position 2 is out of the board region, append current laser path to laser paths list and end path.
                     lazer_paths.append(lazer_path)
                     break
                 elif board_state[inv_pt2[1]][inv_pt2[0]] == "A" or board_state[inv_pt2[1]][inv_pt2[0]] == "B":
+                # When investigating position 2 has either a reflective (A) or an opaque (B) block, append current laser path to laser paths list and end path.
                     lazer_paths.append(lazer_path)
                     break
-                else: # "o" or "x" or "C"
-                    lazer_direction.append(reflected_dir)
-                    lazer_path.append((current_pt[0]+lazer_direction[-1][0], current_pt[1]+lazer_direction[-1][1]))
+                else: 
+                # When investigating position 2 either is blank (o or x) or a refractive (C) block
+                    lazer_direction.append(reflected_dir) # Next laser point would have the reflected direction.
+                    lazer_path.append((current_pt[0]+lazer_direction[-1][0], current_pt[1]+lazer_direction[-1][1])) # Next laser point position.
+                    if len(lazer_path) > 50:
+                        # If the lazer path length is higher than 50, it assumes that it is in an infinite loop and breaks out of the loop
+                        lazer_paths.append(lazer_path)
+                        break
                     
-        # print("start_points2:",start_points)                    
-    return lazer_paths
-
-#Lazer_Paths = Lazer_paths("yarn_5.bff", board_state)                   
+    return lazer_paths                   
                 
-def target_point_chk(filename, lazer_paths, targets): # Not done yet...
+def target_pt_chk(filename, lazer_paths, target_points):
     '''
-    A function to check if the laser path goes through the target points
+    A function to check if the laser path(s) goes through the target points
     
     Parameters
     ----------
-    filename : TYPE
-        DESCRIPTION.
-    lazer_paths : TYPE
-        DESCRIPTION.
-
+    filename : *str*
+        Name of board file.
+    lazer_paths : *list of lists*
+        Laser path(s) to be checked.
+    target_points : *list of tuples*
+        Target points to be checked if they are all included in the laser path(s).
+    
     Returns
     -------
-    target_points : TYPE
-        DESCRIPTION.
-
+    not result : *bool*
+        False: If all targets points are included in the laser path(s).
+        True: If any one of the targets are not included in the lase path(s).
+        
     '''
-    # with open(filename, 'r') as bff:
-    #     lines = bff.readlines()
-    
-    # target_points = []
-    # for line1 in lines:
-    #     line1 = line1.strip()
-        
-    #     if line1.startswith("P"):
-    #         target_point = (int(line1[2]),int(line1[4]))
-    #     else:   # Why are these 'else' lines neccessary?
-    #         continue
-        
-    #     target_points.append(target_point)
-               
-    #print(target_points)
-    
-    # #lazer_paths = [[(6,9)],[(9,2)]]
-    # lazer_paths = [[(1,2),(5,4)], [(6,3)]]
-    target_points = targets
-    
     lazer_pts =[]
     for list in lazer_paths:
         for tuple in list:
             lazer_pts.append(tuple)
-    #print(lazer_pts)        
+           
     result = all(tuple in lazer_pts for tuple in target_points)
     
-    #print(result)   
     return  not result
 
-# board = LazorBoard("yarn_5.bff")
-# board_state = board_state("yarn_5.bff", board)
-# Lazer_Paths = Lazer_paths("yarn_5.bff", board_state)
-# result = target_point_chk("yarn_5.bff",Lazer_Paths)
+def Lazor_solver(filename):
+    '''
+    Solves the Lazor game of the given board format with 'filename'.
+    
+    Parameters
+    ----------
+    filename : *str*
+        Name of board file.
 
-def Lazor_solution(filename):
+    Returns
+    -------
+    curr_board : *list of lists*
+        The answer board state with the configured blocks.
+    "board_name"+"_answer.txt": Saves the answer in a text file.
 
-    # filename = "yarn_5.bff"
+    '''
     board = Board(filename)
-
-# curr_board = board_state(filename, board)
-# # curr_board = [[' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'A', ' ', 'o', ' ', 'A', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'o', ' ', 'o', ' ', 'o', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', 'A', ' ', 'C', ' ', 'o', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-# lazer_paths = Lazer_paths(filename, curr_board)
-# flag = target_point_chk(filename, lazer_paths)
+    target_points = board.get_targets()
 
     iteration = 0
-    result = True
-    while result:
-        # starting_points = []
-        curr_board = board.board
-        # print(curr_board)
-        starting_points = board.lazors.copy()
-        # print("lazors:",lazors)
-        target_points = board.targets
+    result = True 
+    while result: # Result will be changed to false when the answer board is found and the while loop ends.
+        curr_board = board.get_board()
+        starting_points = board.get_lazors()
         lazer_paths = Lazer_paths(filename, curr_board, starting_points)
-        # print(lazer_paths)
-        result = target_point_chk(filename, lazer_paths, target_points)
+        result = target_pt_chk(filename, lazer_paths, target_points)
         iteration += 1
         print(iteration)
         
-    with open(filename +'_answer.txt', 'w') as answer_file:
-            
+    filename = filename.split('.')[0]    
+    with open(filename +'_answer.txt', 'w') as answer_file:           
         for list in curr_board:
             for i in list:
                 answer_file.write(i)
-            answer_file.write('\n')
-            
+            answer_file.write('\n')           
     return curr_board
 
     
-# if __name__ == "__main__":    
-#     # board = Board("tiny_5.bff")
-#     # curr_board = board.board_state()
-#     # lazors = board.lazors
-#     # print("lazors2:",lazors)
-#     # targets = board.targets
-    
-answer = Lazor_solution("tiny_5.bff")
-    
-#     # print(curr_board)
-#     # print(lazors)
-#     # print(targets)
-
-
-if __name__ == '__main__':
-    board = Board("tiny_5.bff")
-    game_board = board.board
-    lazor = board.lazors
-    targets = board.targets
-    print(game_board)
-    print(lazor)
-    print(targets)
+if __name__ == "__main__":     
+    answer = Lazor_solver("yarn_5.bff")
